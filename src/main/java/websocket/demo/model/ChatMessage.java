b7952d53e4c2f203815703c0ca8e37ab5221fd39
youssefjn <youssefjneyah@gmail.com>
2023-01-18 09:52:19 +0100

    first commit

---
+++ src/main/java/websocket/demo/model/ChatMessage.java
@@ -0,0 +1,18 @@
+package websocket.demo.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ChatMessage {
+    private String content;
+    private String sender;
+    private MessageType type;
+
+    public enum MessageType{
+        CHAT,
+        JOIN,
+        LEFT
+    }
+}
